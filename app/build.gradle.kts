8a66b389b03a552528c528ad1c34df0d17cfa5b8
nick <92070739+u0@users.noreply.github.com>
2023-10-11 00:55:56 -0400

    firebase implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("com.android.application")
+
+    // Add the Google services Gradle plugin
     id("com.google.gms.google-services")
 }
 
@@ -34,13 +36,16 @@ android {
 
 dependencies {
 
-    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    implementation("com.google.firebase:firebase-analytics")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+
+    // Add the dependency for the Firebase SDK for Google Analytics
+    implementation("com.google.firebase:firebase-analytics")
 }
