f4e18d5bbb7613f2d7057769ef501ca4564ae299
eracosta-codes <67158077+eracosta-codes@users.noreply.github.com>
2023-11-10 10:52:20 -0500

    Update HillClimb.java

--- app/src/main/java/com/example/cyclingclub/HillClimb.java
+++ app/src/main/java/com/example/cyclingclub/HillClimb.java
@@ -1,4 +1,8 @@
 package com.example.cyclingclub;
 
 public class HillClimb extends Event{
+  HillClimb(double height){
+    super(getKey, getName, getLocation,"HillClimb",getTime, getDuration);
+    height = this.height;
+  }
 }
