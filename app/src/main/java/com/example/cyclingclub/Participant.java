f1b77e1d4dc9bce3c1c05704d3bbd5a5c03a63d8
Edgar Acosta <er.acosta@gmail.com>
2023-11-22 17:20:59 -0500

    Revert "Update Profile"
    This reverts commit d5b447c2cdd703f82581927cc77cd2ce38bdd97e.

---
+++ app/src/main/java/com/example/cyclingclub/Participant.java
@@ -0,0 +1,38 @@
+package com.example.cyclingclub;
+
+import java.util.LinkedList;
+
+public class Participant extends User {
+    Participant newUser;
+    Event newRegistration;
+    LinkedList<Event> registeredEvents;
+    public Participant(String email, String username, String password, String salt) {
+        super(email, username, "Participant", password, salt);
+    }
+
+    public void registerForEvent(){
+        newRegistration = new Event();
+        newRegistration.users.add(newUser);
+        registeredEvents.add(newRegistration);
+    }
+
+    /*
+    public String searchByLocation(){
+        return Event.getLocation();
+    }
+
+    public String searchByType(){
+        return Event.getType();
+    }
+    public String searchByName(){
+        return Event.getName();
+    }
+    public String searchByDate(){ return Event.getDate(); }
+
+    public void viewEvents(){
+
+    }
+
+
+     */
+}
