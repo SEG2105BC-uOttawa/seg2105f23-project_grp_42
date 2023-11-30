834a0536c71c05d444c9a3e2d79a3a829390b11f
Ketan Kumar <kkuma074@uottawa.ca>
2023-11-29 22:25:14 -0500

    Starting UNIT TEST CASES

---
+++ app/src/test/java/com/example/cyclingclub/RegisterTest.java
@@ -0,0 +1,27 @@
+package com.example.cyclingclub;
+
+//import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+
+//@RunWith(JUnit4.class)
+//
+//    public class RegisterTest {
+//    private Participant participant;
+//    private User user;
+//
+//    public void setUp() {
+//        // Initialize Participant object or mock it as needed
+//        participant = new Participant();
+//        user=new User();
+//    }
+//    @Test
+//    public void testParticipantRegistration() {
+//        assertThat(participant.registerForEvent);
+//    }
+//    }
