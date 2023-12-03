9e475ddb0b145ce9f6f0e19dc7ab80c61bf1a20f
Ketan Kumar <kkuma074@uottawa.ca>
2023-12-03 12:59:42 -0500

    Adding Rough Code for first Unit Test

--- app/src/test/java/com/example/cyclingclub/RegisterTest.java
+++ app/src/test/java/com/example/cyclingclub/RegisterTest.java
@@ -1,6 +1,6 @@
 package com.example.cyclingclub;
 
-//import static org.junit.Assert.*;
+import static org.junit.Assert.*;
 
 import static org.junit.Assert.assertThat;
 
@@ -9,19 +9,19 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 
-//@RunWith(JUnit4.class)
-//
-//    public class RegisterTest {
-//    private Participant participant;
-//    private User user;
-//
-//    public void setUp() {
-//        // Initialize Participant object or mock it as needed
-//        participant = new Participant();
-//        user=new User();
-//    }
-//    @Test
-//    public void testParticipantRegistration() {
-//        assertThat(participant.registerForEvent);
-//    }
-//    }
+@RunWith(JUnit4.class)
+
+    public class RegisterTest {
+    private Participant participant;
+    private User user;
+
+    public void setUp() {
+        // Initialize Participant object or mock it as needed
+        participant = new Participant("admin",);
+        //user=new User();
+    }
+    @Test
+    public void testParticipantRegistration() {
+        assertThat(participant.registerForEvent);
+    }
+    }
