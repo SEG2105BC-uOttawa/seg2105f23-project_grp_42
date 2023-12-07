7e2dcde3521e7804fed385656f08b75fc3f0c93d
Edgar Acosta <er.acosta@gmail.com>
2023-12-06 22:56:58 -0500

    finished testcases

--- app/src/test/java/com/example/cyclingclub/AppUnitTest.java
+++ app/src/test/java/com/example/cyclingclub/AppUnitTest.java
@@ -1,19 +1,19 @@
 package com.example.cyclingclub;
 
-import android.widget.ArrayAdapter;
-import com.example.cyclingclub.activities.SearchForClub;
-import com.example.cyclingclub.utils.Utils;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AppUnitTest {
     @Test
     public void inputValidation_isCorrect() {
-        Utils utils = Utils.getInstance();
+        InputValidator utils = InputValidator.getInstance();
         assertTrue(utils.isValidString("Test"));
         assertFalse(utils.isValidString(""));
         assertFalse(utils.isValidString("123"));
@@ -28,7 +28,7 @@ public class AppUnitTest {
 
     @Test
     public void eventCreation_isCorrect() {
-        Event event = new Event("key", "eventId", "eventType", "eventDetail", "eventRegion", "eventDate", 1, 10.0, 100, "eventDistance", "eventElevation");
+        Event event = new Event("key", "eventId", "eventType",  "eventDate");
         assertEquals("eventId", event.getId());
         assertEquals("eventType", event.getType());
     }
@@ -48,24 +48,9 @@ public class AppUnitTest {
     }
 
     @Test
-    public void eventRegionAndDate_areCorrect() {
-        Event event = new Event("key", "eventId", "eventType", "eventDetail", "eventRegion", "eventDate", 1, 10.0, 100, "eventDistance", "eventElevation");
-        assertEquals("eventRegion", event.getRegion());
-        assertEquals("eventDate", event.getDate());
-    }
-
-    @Test
-    public void eventDate_isCorrect() throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
-        Date date = sdf.parse("2023-01-01");
-        Event event = new Event("key", "eventId", "eventType", "eventDetail", "eventRegion", sdf.format(date), 1, 10.0, 100, "eventDistance", "eventElevation");
-        assertTrue(sdf.parse(event.getDate()).after(new Date()));
-    }
-
-    @Test
     public void eventTypeKey_isCorrect() {
         EventType eventType = new EventType("key", "typeName", "detail");
-        assertEquals("key", eventType.getKey());
+        assertEquals("key", eventType.getId());
     }
 
     @Test
@@ -75,30 +60,23 @@ public class AppUnitTest {
         assertEquals("salt", user.getSalt());
     }
 
-    @Test
-    public void eventDetailDistanceAndElevation_areCorrect() {
-        Event event = new Event("key", "eventId", "eventType", "eventDetail", "eventRegion", "eventDate", 1, 10.0, 100, "eventDistance", "eventElevation");
-        assertEquals("eventDetail", event.getDetail());
-        assertEquals("eventDistance", event.getDistance());
-        assertEquals("eventElevation", event.getElevation());
-    }
 
     @Test
     public void isValidString_handlesNull() {
-        Utils utils = Utils.getInstance();
+        InputValidator utils = InputValidator.getInstance();
         assertFalse(utils.isValidString(null));
     }
 
     @Test
     public void eventTypeKey_isUnique() {
         EventType eventType1 = new EventType("key", "typeName", "detail");
-        EventType eventType2 = new EventType("key", "typeName", "detail");
-        assertNotEquals(eventType1.getKey(), eventType2.getKey());
+        EventType eventType2 = new EventType("2", "typeName", "detail");
+        assertNotEquals(eventType1.getId(), eventType2.getId());
     }
 
     @Test
     public void isValidString_handlesSpecialCharacters() {
-        Utils utils = Utils.getInstance();
+        InputValidator utils = InputValidator.getInstance();
         assertFalse(utils.isValidString("@#%&*"));
     }
 }
